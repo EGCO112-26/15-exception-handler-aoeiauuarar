bcac39ebf580f5de87875bf2763cd1f3c09809f7
aoeiauuarar <tawangood46@gmail.com>
2026-04-20 08:23:27 +0000

    Updatestd

--- main.cpp
+++ main.cpp
@@ -1,18 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void check0(int );
+
 int main(){
 	int x,y;
 	double d;
-	cin>>x>>y;
-	if(cin.fail()){
-		cerr<<"Incorrect type entered"; return 1;
-	}
-	if(abs(x)>1000 ||abs(y)>1000){
-		cerr<<"alue out of range"; return 1;
-	}
-	if (y==0) {
-		cerr<< "Error divide by zero"; return 1;
-	}
+	int a=1;
+do{
+	try{
 
-	d=(double) x/y;
-	cout<< "The result is" <<d;
+		int i ;
+		double myarray;
+		for(i=0;i<100000;i++){
+			cout<<"Allocating memory ...."<<i<<endl;
+			myarray= new double[50000000];
+		}
+		cout<<"Enter 2 num: ";
+		cin>>x>>y;
+		if(cin.fail()){
+			a=0;
+			throw 101;
+		}
+		if(abs(x)>1000 ||abs(y)>1000){
+			a=0;
+			throw"alue out of range";
+		}
+		check0(y);
+		d=(double) x/y;
+		cout<< "The result is " <<d<<endl;
+	}
+	catch(const char* error){
+		cerr<<error<<endl;
+	}
+	catch(const int x){
+		switch(x){
+			case 101: cerr<<"Incorrect type entered"<<endl; break;
+			case 0: cerr<<"Error divide by zero"<<endl; break;
+		}
+		
+		cin.clear();
+		cin.ignore(50,'\n');
+	}
+}while(a);
 	return 0;
+}
+
+void check0(int x){
+	if(x==0){
+		int a=0;
+		throw(x);
+	}
 }
