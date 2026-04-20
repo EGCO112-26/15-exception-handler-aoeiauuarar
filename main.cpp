cad9386bdee5fe36965c599014684156f088bf68
aoeiauuarar <tawangood46@gmail.com>
2026-04-20 09:22:53 +0000

    Updatestd

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <exception>
 using namespace std;
-
 void check0(int );
+void checkFail();
+
+class div0:public exception{
+	public:
+	virtual const char* what() const throw(){
+		return "Error divide by zero";
+	}
+};
+
+class input_fail:public exception{
+	public:
+	virtual const char* what() const throw(){
+		return "Incorrect type entered";
+	}
+};
 
 int main(){
 	int x,y;
@@ -10,39 +25,25 @@ int main(){
 do{
 	try{
 
+		cout<<"Enter 2 num: ";
+		cin>>x>>y;
+		checkFail();
+		if (x < -10000 || x > 1000 || y < -10000 || y > 1000) {
+			throw "Value out of range (-10000 to 1000)";
+        }
+		check0(y);
+		d=(double) x/y;
+		cout<< "The result is " <<d<<endl;
+
 		int i ;
 		double* myarray;
 		for(i=0;i<100000;i++){
 			cout<<"Allocating memory ...."<<i<<endl;
 			myarray= new double[50000000];
 		}
-		cout<<"Enter 2 num: ";
-		cin>>x>>y;
-		if(cin.fail()){
-			a=0;
-			throw 101;
-		}
-		if(abs(x)>1000 ||abs(y)>1000){
-			a=0;
-			throw"alue out of range";
-		}
-		check0(y);
-		d=(double) x/y;
-		cout<< "The result is " <<d<<endl;
 	}
-	catch(const char* error){
-		cerr<<error<<endl;
-	}
-	catch(const int x){
-		switch(x){
-			case 101: cerr<<"Incorrect type entered"<<endl; break;
-			case 0: cerr<<"Error divide by zero"<<endl; break;
-		}
-		
-		cin.clear();
-		cin.ignore(50,'\n');
-	}
-	catch(exception &e){
+
+	catch(exception& e){
 		cout<<e.what()<<endl;
 		a=0;
 	}
@@ -51,7 +52,17 @@ do{
 }
 
 void check0(int x){
+	div0 e;
 	if(x==0){
-		throw(x);
+		throw e;
 	}
+}
+
+void checkFail() {
+	input_fail e;
+    if (cin.fail()) {
+		cin.clear();
+		cin.ignore(50,'\n');
+        throw e;
+    }
 }
